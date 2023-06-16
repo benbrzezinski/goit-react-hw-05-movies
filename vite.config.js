1957ba081f4d9ecf0a7da9acbb93b3f491b1da83
Beniamin Brzeziński <benek9311@wp.pl>
2023-06-16 16:01:27 +0200

    Added trailing slash to base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-hw-05-movies",
+  base: "/goit-react-hw-05-movies/",
   plugins: [react()],
 });
